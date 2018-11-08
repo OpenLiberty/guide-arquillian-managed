0b44c583eaa39a21d3ca07a9a6182469e532e887
Eveline Cai <evelinec@ca.ibm.com>
2018-11-08 14:54:34 -0500

    Addressed all feedback issues

--- finish/src/main/java/org/arquillian/example/PhraseBuilder.java
+++ finish/src/main/java/org/arquillian/example/PhraseBuilder.java
@@ -1,12 +1,22 @@
-/*
- * Copyright 2012, 2018, IBM Corporation, Red Hat Middleware LLC, and individual contributors
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2018 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *
+ *******************************************************************************
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -14,7 +24,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ ******************************************************************************/
+// end::copyright[]
 package org.arquillian.example;
 
 import java.text.MessageFormat;
--- finish/src/test/java/it/io/openliberty/guides/arquillian/example/GreeterIT.java
+++ finish/src/test/java/it/io/openliberty/guides/arquillian/example/GreeterIT.java
@@ -1,12 +1,22 @@
-/*
- * Copyright 2012, 2018, IBM Corporation, Red Hat Middleware LLC, and individual contributors
+// tag::copyright[]
+/*******************************************************************************
+ * Copyright (c) 2018 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *
+ *******************************************************************************
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -14,14 +24,12 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-package org.arquillian.example;
+ ******************************************************************************/
+// end::copyright[]
+package it.io.openliberty.guides.arquillian.example;
 
 import org.arquillian.example.Greeter;
 import org.arquillian.example.PhraseBuilder;
-
-import java.io.File;
-
 import javax.inject.Inject;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -31,15 +39,18 @@ import org.junit.Test;
 import org.junit.Assert;
 import org.junit.runner.RunWith;
 
+// tag::comment[]
 /**
  * @author <a href="http://community.jboss.org/people/dan.j.allen">Dan Allen</a>
  */
+// end::comment[]
 @RunWith(Arquillian.class)
 public class GreeterIT {
 
     @Deployment
     public static JavaArchive createDeployment() {
-        JavaArchive archive = ShrinkWrap.create(JavaArchive.class).addClasses(Greeter.class, PhraseBuilder.class);
+        JavaArchive archive = ShrinkWrap.create(JavaArchive.class)
+                                        .addClasses(Greeter.class, PhraseBuilder.class);
         return archive;
     }
 
