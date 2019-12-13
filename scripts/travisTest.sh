ececb666b293f2a3f4ae869b95e1da74e83e6c9e
evelinec <evelinec@ca.ibm.com>
2019-12-13 14:36:17 -0500

    Simplied pom version of the guide

--- scripts/travisTest.sh
+++ scripts/travisTest.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -euxo pipefail
 
-mvn clean package liberty:create liberty:install-feature liberty:deploy
+mvn clean package
+
+mvn liberty:create liberty:install-feature liberty:deploy
 
 # Run configure arquillian goal to create the arquillian.xml file
 mvn liberty:configure-arquillian
 
-mvn liberty:start
+# The arquillian tests manage the lifecycle of the application server, ie, start/stop
 mvn failsafe:integration-test
 
-mvn liberty:stop
 mvn failsafe:verify
