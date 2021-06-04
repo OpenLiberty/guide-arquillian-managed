d0cafd0f9895bc046232e6a7f1481128c22b97ea
Gilbert Kwan <gkwan@ca.ibm.com>
2021-06-04 12:07:41 -0400

    Revert "Merge staging to prod - Version update (#103)"

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -2,33 +2,15 @@
 set -euxo pipefail
 
 # Test without liberty:deploy
-mvn -Dhttp.keepAlive=false \
-    -Dmaven.wagon.http.pool=false \
-    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -q clean package
-mvn -Dhttp.keepAlive=false \
-    -Dmaven.wagon.http.pool=false \
-    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -q liberty:create liberty:install-feature
-mvn -Dhttp.keepAlive=false \
-    -Dmaven.wagon.http.pool=false \
-    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -q liberty:configure-arquillian
+mvn -q clean package
+mvn -q liberty:create liberty:install-feature
+mvn -q liberty:configure-arquillian
 mvn failsafe:integration-test
 mvn failsafe:verify
 
 # Test with liberty:deploy
-mvn -Dhttp.keepAlive=false \
-    -Dmaven.wagon.http.pool=false \
-    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -q clean package
-mvn -Dhttp.keepAlive=false \
-    -Dmaven.wagon.http.pool=false \
-    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -q liberty:create liberty:install-feature liberty:deploy
-mvn -Dhttp.keepAlive=false \
-    -Dmaven.wagon.http.pool=false \
-    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -q liberty:configure-arquillian
+mvn -q clean package
+mvn -q liberty:create liberty:install-feature liberty:deploy
+mvn -q liberty:configure-arquillian
 mvn failsafe:integration-test
 mvn failsafe:verify
