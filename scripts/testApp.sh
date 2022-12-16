600ac06127aa636dcf60ac9e0749820444f09bcb
Jakub Pomykala <jakub.pomykala@hotmail.co.uk>
2022-12-16 17:52:49 +0000

    adding -ntp to maven

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -2,33 +2,33 @@
 set -euxo pipefail
 
 # Test without liberty:deploy
-mvn -Dhttp.keepAlive=false \
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q clean package
-mvn -Dhttp.keepAlive=false \
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q liberty:create liberty:install-feature
-mvn -Dhttp.keepAlive=false \
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q liberty:configure-arquillian
-mvn failsafe:integration-test
-mvn failsafe:verify
+mvn -ntp failsafe:integration-test
+mvn -ntp failsafe:verify
 
 # Test with liberty:deploy
-mvn -Dhttp.keepAlive=false \
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q clean package
-mvn -Dhttp.keepAlive=false \
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q liberty:create liberty:install-feature liberty:deploy
-mvn -Dhttp.keepAlive=false \
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q liberty:configure-arquillian
-mvn failsafe:integration-test
-mvn failsafe:verify
+mvn -ntp failsafe:integration-test
+mvn -ntp failsafe:verify
